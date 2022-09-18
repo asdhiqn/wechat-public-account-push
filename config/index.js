59c62cc0c1fba7b925558b552340f6b20b8f9c34
asdhiqn <113715714+asdhiqn@users.noreply.github.com>
2022-09-18 17:20:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "olcwm6FnKC6X5yvHPzciSaSlAEoQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "fbxs9_u5O2k34JnT184eUFjZAZrB_Bicl_4RmlVEWIs",
+      useTemplateId: "ZfjtXGVa7QQEvWwsyhqZfjBFEwCt0DM6g7FkyYDC2zU",
       // 所在省份
       province: "广东",
       // 所在城市
@@ -137,7 +137,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "fbxs9_u5O2k34JnT184eUFjZAZrB_Bicl_4RmlVEWIs",
+  CALLBACK_TEMPLATE_ID: "ZfjtXGVa7QQEvWwsyhqZfjBFEwCt0DM6g7FkyYDC2zU",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
